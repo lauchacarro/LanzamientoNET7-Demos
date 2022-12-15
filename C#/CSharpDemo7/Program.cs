3436382c8b40a04322a6ef766047bde249e675bb
Lautaro Carro <lcarro@algeiba.com>
2022-12-15 16:09:10 -0300

    Agrego Demo de Blazor y C#

--- C#/CSharpDemo7/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using CSharpDemo7;
-
-Person person = new() { FirstName = "Foo", LastName = "Bar" };
-
-
-Console.WriteLine("Hello, World!");
